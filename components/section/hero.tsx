7add7fbc8ac3cea7fa1ea2f78cc75a96b4a40ef4
Naol bm <profesornaoltena@gmail.com>
2024-07-29 22:23:35 +0300

    copy added

--- components/section/hero.tsx
+++ components/section/hero.tsx
@@ -1,8 +1,23 @@
+'use client'
+
+import { Copy, Computer } from "lucide-react";
+
 export default function StarterDummy() {
-  return (
-    <div>
-          Next.js + Shadcn + Tailwind
-          <br/> link:  
+  const txt = 'git clone https://github.com/Naol-bm/nextjs-shadcn-starter-kit-koo.git'
+
+  const copyText = ()=>{
+    navigator.clipboard.writeText(txt)
+        alert('txt copied to z clipboard')
+  }
+    return (
+    <div className="flex justify-between py-[2.5%] bg-primary text-secondary w-full gap-8 items-center flex-col h-screen">
+          <div className="text-primary-foreground/5">YeneKoo</div>
+          <div className="font-bold text-4xl flex justify-center items-center flex-col gap-6">
+          <Computer size={'150px'}/>
+
+          Next.js + Shadcn + Tailwind 🫣 
+          </div>
+          <div className="flex gap-2 bg-primary-foreground/20 p-2 rounded-md">{txt} <Copy className="hover:cursor-pointer" onClick={copyText}/></div>  
     </div>
   );
 }
